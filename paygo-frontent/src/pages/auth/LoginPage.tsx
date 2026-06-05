ba131a079596d066ced937e18ac1164b2813c4c2
sonjinile <sy7521012@naver.com>
2026-06-05 11:43:19 +0900

    feat: 비밀번호 input 추가 및 커서 커스텀 blink, 입력 시 동일한 텍스트 커스텀 기능 추가

--- paygo-frontent/src/pages/auth/LoginPage.tsx
+++ paygo-frontent/src/pages/auth/LoginPage.tsx
@@ -1,7 +1,12 @@
 import AuthLayout from "../../components/layout/AuthLayout.tsx";
+import { useState, useRef } from "react";
 import { IconCreditCard, IconWallet, IconShoppingCart, IconChartBar } from "@tabler/icons-react";
 
 function LoginPage(){
+    const [password, setPassword] = useState('');
+    const [focused, setFocused] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
+
     return (
         <AuthLayout>
             <div className="flex h-screen min-w-[1280px]"> {/* 1280px 이상은 내부 컴포넌트(1280px 이하)에 대해 찌그러지지 않음 */}
@@ -82,7 +87,38 @@ function LoginPage(){
                                 이메일
                             </div>
                             <div>
-                                <input type="text" placeholder="example@email.com" className="font-medium text-gray-600 pl-3.5 w-[400px] h-[40px] rounded-md border border-[#D9D9D9]"/>
+                                <input type="text" placeholder="example@email.com" className="font-medium text-gray-600 pl-3.5 w-[400px] h-[40px] leading-[40px] rounded-md outline-none border border-[#D9D9D9]"/>
+                            </div>
+                        </div>
+                        <div className="mt-5 text-[#bdb6b1] font-medium text">
+                            <div className="mb-2">
+                                비밀번호
+                            </div>
+                            <div className="relative w-[400px] h-[40px]"
+                            onClick={() => inputRef.current?.focus()}>
+                                {/* 실제 입력받는 투명 input */}
+                                <input
+                                    ref={inputRef}
+                                    type="text"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    onFocus={() => setFocused(true)} // 클릭하면 focused = true
+                                    onBlur={() => setFocused(false)} // 다른 곳 누르면 focused = false
+                                    className="absolute inset-0 w-full h-full opacity-0 cursor-text"
+                                />
+                                {/* 보여주는 div */}
+                                <div className="flex items-center h-full pl-3.5 border border-[#D9D9D9] rounded-md text-gray-600">
+                                    <span className="text-[12px] tracking-widest text-gray-600">
+                                        {'●'.repeat(password.length)}
+                                    </span>
+                                    {/* 가짜 커서 */}
+                                    {focused && (
+                                        <span className="inline-block w-[1px] h-[22px] bg-gray-600 ml-[1px] animate-blink"/>
+                                    )}
+                                    {!password && !focused && (
+                                        <span className="absolute text-[12px] tracking-widest text-gray-400">●●●●●●●</span>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     </div>
