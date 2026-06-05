29c86dc10739e5754b2de28b522a4bb49ae2f5d0
sonjinile <sy7521012@naver.com>
2026-06-05 15:14:01 +0900

    chore: prettier 설정 추가

--- paygo-frontent/src/router/index.tsx
+++ paygo-frontent/src/router/index.tsx
@@ -1,6 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '../pages/auth/LoginPage.tsx';
 
-export const router = createBrowserRouter([
-    { path : '/login', element: <LoginPage /> }
-])
+export const router = createBrowserRouter([{ path: '/login', element: <LoginPage /> }]);
