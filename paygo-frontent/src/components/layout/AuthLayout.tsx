ef167708194b521aec63c2a141a5c8e7de498360
sonjinile <sy7521012@naver.com>
2026-06-10 06:24:05 +0900

    feat: auth 레이아웃 추가

--- paygo-frontent/src/components/layout/AuthLayout.tsx
+++ paygo-frontent/src/components/layout/AuthLayout.tsx
@@ -3,8 +3,10 @@ import * as React from 'react';
 function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="h-screen w-screen bg-gray-50">
-            {/* h-screen은 상하로 꽉차게. 100vh / w-screen은 좌우로 꽉차게. 100vw*/}
-            {children}
+            {/* 1280px 이상은 내부 컴포넌트(1280px 이하)에 대해 찌그러지지 않음 */}
+           <div className="flex h-screen min-w-[1280px]">
+                {children}
+            </div>
         </div>
     );
 }
--- paygo-frontent/src/router/index.tsx
+++ paygo-frontent/src/router/index.tsx
@@ -1,4 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '../pages/auth/LoginPage.tsx';
+import SignupPage from '../pages/auth/Signup.tsx';
 
-export const router = createBrowserRouter([{ path: '/login', element: <LoginPage /> }]);
+export const router = createBrowserRouter([
+    { path: '/login', element: <LoginPage /> },
+    { path: '/signup', element: <SignupPage /> },
+]);
