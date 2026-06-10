dd01b184ba5a3a8c1ae4e8065044e436d79cd0dd
sonjinile <sy7521012@naver.com>
2026-06-10 09:46:17 +0900

    refactor: SignupPage 파일명 변경으로 인한 라우터 index.tsx 코드 수정

--- paygo-frontent/src/router/index.tsx
+++ paygo-frontent/src/router/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '../pages/auth/LoginPage.tsx';
-import SignupPage from '../pages/auth/Signup.tsx';
+import SignupPage from '../pages/auth/SignupPage.tsx';
 
 export const router = createBrowserRouter([
     { path: '/login', element: <LoginPage /> },
