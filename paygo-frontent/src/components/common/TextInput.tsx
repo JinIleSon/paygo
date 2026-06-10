8d3883e04605810d21b350f73fb6251f482eee0d
sonjinile <sy7521012@naver.com>
2026-06-11 06:50:32 +0900

    feat: TextInput className Props 추가

--- paygo-frontent/src/components/common/TextInput.tsx
+++ paygo-frontent/src/components/common/TextInput.tsx
@@ -4,9 +4,10 @@ import { cn } from '../../lib/utils';
 interface TextInputProps {
     value: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    className?: string;
 }
 
-function TextInput({ value, onChange, placeholder, ...props }) {
+function TextInput({ value, onChange, placeholder, className, ...props }) {
     const base =
         'w-full leading-[40px] pl-3.5 h-[40px] rounded-md outline-none bg-white border border-[#d9d9d9]';
 
@@ -16,7 +17,7 @@ function TextInput({ value, onChange, placeholder, ...props }) {
             value={value}
             onChange={onChange}
             placeholder={placeholder}
-            className={cn(base, `font-medium text-gray-600`)}
+            className={cn(base, `font-medium text-gray-600`, className)}
             {...props}
         />
     );
