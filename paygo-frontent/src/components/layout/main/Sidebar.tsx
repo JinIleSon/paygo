6efd69c030abcb99d91430af104021c71222f57a
sonjinile <sy7521012@naver.com>
2026-06-19 06:50:11 +0900

    chore: layout 폴더에 main 폴더 추가

---
+++ paygo-frontent/src/components/layout/main/Sidebar.tsx
@@ -0,0 +1,9 @@
+import type React from "react";
+
+function Sidebar({ children } : { children : React.ReactNode }) {
+    <div className="w-[200px] h-[100px] border border-b">
+
+    </div>
+}
+
+export default Sidebar;
