b40f50a87bfacfd4b2281a71a161149e99fcb621
sonjinile <sy7521012@naver.com>
2026-06-22 07:02:52 +0900

    feat: 사이드바 레이아웃 및 로고, 지갑부터 계정까지 아이콘 및 텍스트 배치 완료

--- paygo-frontent/src/components/layout/main/Sidebar.tsx
+++ paygo-frontent/src/components/layout/main/Sidebar.tsx
@@ -1,9 +1,90 @@
 import type React from "react";
+import { IconCreditCard, IconWallet, IconPlus, IconList, IconShoppingBag, IconShoppingCart, IconPackage, IconUser, IconSettings, IconWall } from '@tabler/icons-react';
 
 function Sidebar({ children } : { children : React.ReactNode }) {
-    <div className="w-[200px] h-[100px] border border-b">
-
-    </div>
+    return (
+        <div className="w-[240px] h-screen border border-[#D9D9D9]">
+            <div className="flex items-center p-6 gap-3 text-2xl font-bold">
+                <div className="w-[40px] h-[40px] rounded-xl bg-[#6266f1] flex items-center justify-center">
+                    <IconCreditCard size={23} color="white" />
+                </div>
+                <div className="">
+                    <span className="text-[#6266f1]">Pay</span>
+                    <span>go</span>
+                </div>
+            </div>
+            <div className="border-b border-[#D9D9D9]"></div>
+            <div className="p-6 text-[gray]">
+                <div className="font-medium text-[18px]">
+                    지갑
+                </div>
+                <div className="flex items-center gap-3 py-2.5 pt-5">
+                    <IconWallet size={20} color="gray"/>
+                    <div>
+                        지갑 홈
+                    </div>
+                </div>
+                <div className="flex items-center gap-3 py-2.5">
+                    <IconPlus size={20} color="gray"/>
+                    <div>
+                        충전
+                    </div>
+                </div>
+                <div className="flex items-center gap-3 py-2.5">
+                    <IconList size={20} color="gray"/>
+                    <div>
+                        거래내역
+                    </div>
+                </div>
+            </div>
+            <div className="border-b border-[#D9D9D9] mt-[-6px]"></div>
+            <div className="p-6 text-[gray]">
+                <div className="font-medium text-[18px]">
+                    쇼핑
+                </div>
+                <div className="flex items-center gap-3 py-2.5 pt-5">
+                    <IconShoppingBag size={20} color="gray"/>
+                    <div>
+                        상품 목록
+                    </div>
+                </div>
+                <div className="flex items-center gap-3 py-2.5">
+                    <IconShoppingCart size={20} color="gray"/>
+                    <div>
+                        장바구니
+                    </div>
+                </div>
+                <div className="flex items-center gap-3 py-2.5">
+                    <IconPackage size={20} color="gray"/>
+                    <div>
+                        주문내역
+                    </div>
+                </div>
+            </div>
+            <div className="border-b border-[#D9D9D9] mt-[-6px]"></div>
+            <div className="p-6 text-[gray]">
+                <div className="font-medium text-[18px]">
+                    계정
+                </div>
+                <div className="flex items-center gap-3 py-2.5 pt-5">
+                    <IconUser size={20} color="gray"/>
+                    <div>
+                        마이페이지
+                    </div>
+                </div>
+                <div className="flex items-center gap-3 py-2.5">
+                    <IconSettings size={20} color="gray"/>
+                    <div>
+                        설정
+                    </div>
+                </div>
+            </div>
+            <div className="border-b border-[#D9D9D9] mt-[-6px]"></div>
+            <div className="mt-auto border-t border-[#D9D9D9]">
+                손진일
+            </div>
+        </div>
+    );
 }
 
 export default Sidebar;
--- paygo-frontent/src/router/index.tsx
+++ paygo-frontent/src/router/index.tsx
@@ -4,6 +4,7 @@ import SignupPage from '../pages/auth/SignupPage.tsx';
 import TermsOfServicePage from '../pages/auth/terms/TermsOfServicePage.tsx';
 import PrivacyConsentPage from '../pages/auth/terms/PrivacyConsentPage.tsx';
 import MarketingConsentPage from '../pages/auth/terms/MarketingConsentPage.tsx';
+import Sidebar from '../components/layout/main/Sidebar.tsx';
 
 export const router = createBrowserRouter([
     { path: '/login', element: <LoginPage /> },
@@ -11,4 +12,5 @@ export const router = createBrowserRouter([
     { path: '/terms-of-service', element: <TermsOfServicePage /> },
     { path: '/privacy-consent', element: <PrivacyConsentPage /> },
     { path: '/marketing-consent', element: <MarketingConsentPage /> },
+    { path: '/sidebar', element: <Sidebar />},
 ]);
