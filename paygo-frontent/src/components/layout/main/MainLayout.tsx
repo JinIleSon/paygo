9e24ae673d2116f644379030b1c2269f652284f7
sonjinile <sy7521012@naver.com>
2026-06-25 13:00:01 +0900

    feat: MainLayout 전체 레이아웃 크기 AuthLayout과 동일하게 min-w-[1280px]로 고정. 바뀌는 내용물 부분은 max-w-[1000px]로 끝까지 늘어나지 않도록 변경

--- paygo-frontent/src/components/layout/main/MainLayout.tsx
+++ paygo-frontent/src/components/layout/main/MainLayout.tsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom';
 function MainLayout() {
     return (
         // h-screen: 전체 높이 고정 (main 스크롤을 위해 필요)
-        <div className="flex h-screen">
+        <div className="flex h-screen min-w-[1280px]">
             <Sidebar />
             {/* flex-1: Sidebar 제외한 나머지 가로 공간 차지 */}
             {/* min-w-0: 자식 내용물이 넓어져도 flex-1 너비를 초과하지 못하게. min-width:auto를 사용하지 않기 위함. */}
@@ -14,7 +14,11 @@ function MainLayout() {
                 <Topbar />
                 {/* flex-1: Topbar 제외한 나머지 세로 공간 차지 */}
                 {/* overflow-y-auto: 내용물이 넘치면 세로 스크롤 */}
-                <main className="px-8 py-6.5 flex-1 overflow-y-auto bg-[#F6F5F8]"><Outlet /></main>
+                <main className="flex-1 overflow-y-auto bg-[#F6F5F8]">
+                    <div className="max-w-[1000px] mx-auto px-8 py-6.5">
+                        <Outlet />
+                    </div>
+                </main>
             </div>
         </div>
     );
