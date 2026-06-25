002e7b8144abc99d645f9f7be315596b70a8ca1e
sonjinile <sy7521012@naver.com>
2026-06-25 13:00:31 +0900

    feat: 지갑 홈 내 지갑 잔액 카드 완성

--- paygo-frontent/src/pages/wallet/WalletHomePage.tsx
+++ paygo-frontent/src/pages/wallet/WalletHomePage.tsx
@@ -1,7 +1,8 @@
-import MainLayout from "../../components/layout/main/MainLayout";
+import Button from '../../components/common/Button';
+import MainLayout from '../../components/layout/main/MainLayout';
+import { IconPlus, IconArrowRight } from '@tabler/icons-react';
 
-function WalletHomePage(){
-
+function WalletHomePage() {
     const balance = 3842000;
     const pg = 'PG-1234-5678-9012';
 
@@ -11,11 +12,28 @@ function WalletHomePage(){
             <div className="flex gap-8 pr-8">
                 <div className="min-w-1/2">
                     <div className="px-8 py-6.5 bg-[#6266f1] rounded-2xl">
-                        <div className="text-[#E2E3FF]">Paygo 지갑 잔액</div>
-                        <div className="py-3 text-white text-[32px] font-medium">{balance.toLocaleString()}원</div>
-                        <div className="flex items-center gap-3">
-                            <div className="text-[#E2E3FF]">{pg}</div>
-                            <div className="p-5 text-xl rounded-lg border border-white">+ 충전</div> {/* TODO: button 컴포넌트 transition-colors 추가 및 props 추가 */}
+                        <div>
+                            <div className="text-[#E2E3FF]">Paygo 지갑 잔액</div>
+                            <div className="py-3 text-white text-[32px] font-medium">
+                                {balance.toLocaleString()}원
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <div className="text-[#E2E3FF]">{pg}</div>
+                                <div className="flex ml-auto gap-3">
+                                    <Button variant="secondary" className="p-3">
+                                        <div className="flex items-center gap-2">
+                                            <IconPlus size={16} />
+                                            <div>충전</div>
+                                        </div>
+                                    </Button>
+                                    <Button variant="secondary" className="p-3">
+                                        <div className="flex items-center gap-2">
+                                            <IconArrowRight size={16} />
+                                            <div>송금</div>
+                                        </div>
+                                    </Button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
