816e984c3f606c216b962d04ef3381aadc7cfcd2
sonjinile <sy7521012@naver.com>
2026-06-30 06:28:44 +0900

    feat: 충전 페이지 충전 금액 카드 레이아웃 조정

--- paygo-frontent/src/pages/wallet/WalletChargePage.tsx
+++ paygo-frontent/src/pages/wallet/WalletChargePage.tsx
@@ -1,6 +1,21 @@
+import Card from "../../components/common/Card";
+
 function WalletChargePage(){
     return (
         <div>
+            <div className="flex gap-8 pr-8 w-full">
+                <div className="min-w-1/2">
+                    <Card>
+                        <div className="text-[gray] mb-6">충전 금액</div>
+                        <Card className="bg-[#F5F6FF] border-0">
+                            <div className="text-[gray] mb-6">현재 잔액</div>
+                        </Card>
+                    </Card>
+                </div>
+                <div className="min-w-1/2">
+                    <Card></Card>
+                </div>
+            </div>
         </div>
     )
 }
