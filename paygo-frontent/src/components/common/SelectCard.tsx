74bb5ea62335736985e4d3ef6f0f5adb2f9eee54
sonjinile <sy7521012@naver.com>
2026-07-07 07:24:42 +0900

    feat: SelectCard border-transparent로 선택되지 않았거나 hover 시 border 크기만큼 유지되도록 변경. bg 색상 및 border 색상 추가

--- paygo-frontent/src/components/common/SelectCard.tsx
+++ paygo-frontent/src/components/common/SelectCard.tsx
@@ -13,16 +13,17 @@ function SelectCard({variant = 'primary', isSelected, onClick, children, classNa
 
     const variants = {
         primary : 'rounded-full',
-        secondary: 'rounded-md'
+        secondary: 'rounded-xl'
     };
 
     return (
         <button onClick={onClick}
         className={cn(
+            'transition-all duration-200',
             variants[variant], 
             isSelected
-            ? 'border-[#6266F1] bg-[#E4E4FF] text-[#6266F1]'
-            : 'text-[gray] hover:bg-[#F5F6FF] hover:text-[#6266F1] cursor-pointer duration-500',
+            ? 'border border-[#6C6DD3] bg-[#E4E4FF]'
+            : 'border border-transparent border-[#D9D9D9] text-[gray] hover:bg-[#F5F6FF] cursor-pointer duration-500',
             className
         )}
         {...props}
