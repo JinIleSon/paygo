8b17c53b7d50eaf5acd74f2654b1bb0ae3c7d9ff
sonjinile <sy7521012@naver.com>
2026-07-07 20:40:50 +0900

    feat: 거래내역 페이지 내 유형 완료

--- paygo-frontent/src/pages/wallet/WalletHistoryPage.tsx
+++ paygo-frontent/src/pages/wallet/WalletHistoryPage.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import Card from "../../components/common/Card";
+import SelectCard from "../../components/common/SelectCard";
+
 function WalletHistoryPage(){
+
+    const types = [
+        { id: 'all', label: '전체' },
+        { id: 'charge', label: '충전' },
+        { id: 'buy', label: '구매' },
+        { id: 'refund', label: '환불' },
+    ];
+
+    const statement = [
+        { id: 'all', label: '전체' },
+        { id: 'complete', label: '완료' },
+        { id: 'fail', label: '실패' },
+        { id: 'processing', label: '처리중' },
+    ];
+
+    const [selectedType, setSelectedType] = useState('');
+
     return (
         <div>
-
+            <Card>
+                <div className="flex items-center">
+                    <div className="text-[gray] mr-6">유형</div>
+                    <div className="flex gap-3">
+                        {types.map((type) => (
+                            <SelectCard
+                            onClick={() => setSelectedType(type.id)}
+                            isSelected={selectedType === type.id}>
+                                <div>
+                                    {type.label}
+                                </div>
+                            </SelectCard>
+                        ))}
+                    </div>
+                </div>
+            </Card>
         </div>
     );
 }
