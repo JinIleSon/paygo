78678880acba0953372cf0405431b2ce636630da
sonjinile <sy7521012@naver.com>
2026-07-14 13:00:15 +0900

    feat: IconChevronDown 위치 조정 및 hover 시 함께 색이 변경되도록 수정. SelectBox width를 PeriodPicker width와 동일하게 구현

--- paygo-frontent/src/components/common/SelectBox.tsx
+++ paygo-frontent/src/components/common/SelectBox.tsx
@@ -9,16 +9,16 @@ interface SelectBoxProps {
 
 function SelectBox({ children, className, ...props }: SelectBoxProps) {
     const base =
-        'text-gray-400 appearance-none pl-3.5 w-[14.5rem] h-[2.875rem] outline-none border border-[#D1D5DB] hover:text-gray-700 bg-white hover:bg-[#F4F4F4] hover:border-[#C5C8CD] rounded-md cursor-pointer duration-500 transition-colors';
+        'text-gray-400 appearance-none pl-3.5 w-[14.6rem] h-[3rem] outline-none border border-[#D1D5DB] hover:text-gray-700 bg-white hover:bg-[#F4F4F4] hover:border-[#C5C8CD] rounded-md cursor-pointer duration-500 transition-colors';
 
     return (
-        <div className="relative ml-auto">
+        <div className="relative ml-auto group">
             <select className={cn(base, className)} {...props}>
                 {children}
             </select>
             <IconChevronDown
                 size={14}
-                className="absolute top-1/2 right-2 -translate-y-1/2 text-gray-400"
+                className="text-gray-400 group-hover:text-gray-700 pointer-events-none duration-500 absolute top-1/2 right-3.5 -translate-y-1/2"
             />
         </div>
     );
