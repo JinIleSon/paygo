76d686eb9f1af1441ab81ab547191616aaab0c5f
sonjinile <sy7521012@naver.com>
2026-07-14 12:48:05 +0900

    refactor: 공백 통일 및 children에 React.ReactNode로 타입 변경

--- paygo-frontent/src/components/common/Card.tsx
+++ paygo-frontent/src/components/common/Card.tsx
@@ -1,27 +1,26 @@
-import type React from "react";
-import { cn } from "../../lib/utils";
-import type { ReactNode } from "react";
+import type React from 'react';
+import { cn } from '../../lib/utils';
+import type { ReactNode } from 'react';
 
 interface CardProps {
-    variant ?: 'primary' | 'secondary';
+    variant?: 'primary' | 'secondary';
     className?: string;
-    children?: ReactNode;
+    children?: React.ReactNode;
 }
 
-function Card({variant = 'secondary', children, className, ...props}: CardProps){
-    
+function Card({ variant = 'secondary', children, className, ...props }: CardProps) {
     const base = 'px-8 rounded-2xl';
 
     const variants = {
         primary: 'bg-[#6266F1] py-6.5',
-        secondary: 'border border-[#D9D9D9] bg-white py-5'
-    }
-    
-    return(
+        secondary: 'border border-[#D9D9D9] bg-white py-5',
+    };
+
+    return (
         <div className={cn(base, variants[variant], className)} {...props}>
             {children}
         </div>
-    );   
+    );
 }
 
 export default Card;
