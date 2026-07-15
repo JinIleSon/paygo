453d2334dc21336f48e5d7ef5297b6cb0809b084
sonjinile <sy7521012@naver.com>
2026-07-16 07:43:41 +0900

    feat: 상품 상세 페이지 내 목업 데이터 설계 추가 및 레이아웃 추가

--- paygo-frontent/src/pages/shopping/ProductDetailPage.tsx
+++ paygo-frontent/src/pages/shopping/ProductDetailPage.tsx
@@ -1,6 +1,24 @@
+import Card from "../../components/common/Card";
+
 function ProductDetailPage() {
     return (
-        <></>
+        <div>
+            <div className="flex gap-8 pr-8">
+                <div className="min-w-1/2">
+                    <Card className="p-0 h-125 overflow-hidden">
+                        <div className="h-100 bg-[#F5F6FF]">
+
+                        </div>
+                        <div className="px-4.5 pb-3">
+
+                        </div>
+                    </Card>
+                </div>
+                <div className="min-w-1/2">
+                
+                </div>
+            </div>
+        </div>
     );
 }
 
