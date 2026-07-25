513efcd318d9eda2f42c7155e7f583033b05cb04
sonjinile <sy7521012@naver.com>
2026-07-25 09:03:23 +0900

    refactor: 장바구니 Zustand 스토어(useCartStore) 추가

---
+++ paygo-frontent/src/stores/useCartStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import type { CartItem } from "../types/cartItem";
+
+interface CartState {
+    items: CartItem[];
+    addItem: (item: CartItem) => void;
+    removeItem: (productId: number, color: string, size?: number) => void;
+}
+
+export const useCartState = create<CartState>((set) => ({
+    items: [],
+
+    addItem: (item) => 
+        set((state) => ({
+            items: [...state.items, item]
+    })),
+
+    removeItem: (productId, color, size) =>
+        set((state) => ({
+            items: state.items.filter(
+                (i) => !(i.productId === productId && i.color === color && i.size === size)
+            )
+        }))
+}));
