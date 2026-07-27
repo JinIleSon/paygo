94cd501c71c251fb22752ef37b990acd20b397d8
sonjinile <sy7521012@naver.com>
2026-07-27 19:54:46 +0900

    refactor: toast 스토어 오류 코드 수정

--- paygo-frontent/src/stores/useToastStore.ts
+++ paygo-frontent/src/stores/useToastStore.ts
@@ -1,24 +1,23 @@
 import { create } from "zustand";
 import type { Toast } from "../types/toast";
 
-interface ToastState {
-    toasts: Toast[];
+interface ToastStore {
+    toasts: Toast[],
     showToast: (message: string, type?: Toast['type']) => void;
     removeToast: (id: number) => void;
 }
 
-export const useToastStore = create<ToastState>((set) => ({
+export const useToastStore = create<ToastStore>((set) => ({
     toasts: [],
 
     showToast: (message, type = 'success') => {
         const id = Date.now();
 
-        // 새 토스트를 배열 끝에 추가
         set((state) => ({
             toasts: [...state.toasts, { id, message, type }]
         }));
 
-        // 2초 뒤 같은 id를 배열에서 제거
+        // 2초 뒤 state에서 제거
         setTimeout(() => {
             set((state) => ({
                 toasts: state.toasts.filter((t) => t.id !== id)
@@ -26,8 +25,9 @@ export const useToastStore = create<ToastState>((set) => ({
         }, 2000);
     },
 
-    removeToast: (id) => 
+    removeToast: (id) => {
         set((state) => ({
             toasts: state.toasts.filter((t) => t.id !== id)
-        }))
+        }));
+    }
 }));
