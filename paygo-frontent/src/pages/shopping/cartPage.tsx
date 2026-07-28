2f4d08092a03f2e9d11590a247f4d97e95a1c9c6
sonjinile <sy7521012@naver.com>
2026-07-29 07:04:54 +0900

    refactor: 개별/전체 선택 토글 로직 함수 위치 조정

--- paygo-frontent/src/pages/shopping/cartPage.tsx
+++ paygo-frontent/src/pages/shopping/cartPage.tsx
@@ -1,19 +1,35 @@
 import { useState } from "react";
 import Card from "../../components/common/card";
 import { useCartStore } from "../../stores/useCartStore";
-import { et } from "date-fns/locale";
 
 function CartPage() {
+    const items = useCartStore((state) => state.items);
+    const removeItem = useCartStore((state) => state.removeItem);
+    const [selectedIds, setSelectedIds] = useState<number[]>([]);
+
+    // 개별 체크박스 토글
+    const toggleItem = (productId: number) => {
+        setSelectedIds((prev) => 
+            prev.includes(productId)
+                ? prev.filter((id) => productId !== id) // 이미 선택됨 -> 제거
+                : [...prev, productId]                  // 선택 안 됨 -> 추가
+        );
+    };
+
+    // 전체 선택 토글
+    const toggleAll = () => {
+        setSelectedIds((prev) => 
+            prev.length === items.length
+            ? []                                        // 전부 선택됨 -> 전체 해제
+            : items.map((item) => item.productId)       // 아니면 -> 전체 선택
+        );
+    };
+
     const checkedStyle = {
         backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='none'%3E%3Cpath d='M3 8l3.5 3.5L13 5' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E")`,
         backgroundColor: '#6266F1', // 연한 회색빛 보라
         borderColor: '#6266F1', // 테두리도 같이 맞춰주기
     };
-
-    const [checked, setChecked] = useState(false);
-
-    const items = useCartStore((state) => state.items);
-    const removeItem = useCartStore((state) => state.removeItem)
 
     return (
         <div>
