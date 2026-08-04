b54ebe5c520e409ff997b3b72112f2a479b4f8ed
sonjinile <sy7521012@naver.com>
2026-08-04 20:05:26 +0900

    refactor: 쿠폰 타입 expiresAt 필수로 변경

--- paygo-frontent/src/types/coupon.ts
+++ paygo-frontent/src/types/coupon.ts
@@ -4,5 +4,5 @@ export interface Coupon {
     discountType: 'percent' | 'fixed'; // 할인이 정률인지 정액인지
     discountValue: number;
     status: 'active' | 'used' | 'expired'; // 활성, 사용, 만료
-    expiresAt?: string; // 만료일
+    expiresAt: string; // 만료일
 }
