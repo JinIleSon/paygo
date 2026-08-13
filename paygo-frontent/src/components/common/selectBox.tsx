8c60db13c7cbced7ea8c41d4b44492509414184c
sonjinile <sy7521012@naver.com>
2026-08-13 20:17:50 +0900

    refactor: SelectBox 공통 컴포넌트 select props 받을 수 있도록 조정

--- paygo-frontent/src/components/common/selectBox.tsx
+++ paygo-frontent/src/components/common/selectBox.tsx
@@ -1,8 +1,9 @@
 import { IconChevronDown } from '@tabler/icons-react';
 import { cn } from '../../lib/utils';
 import type React from 'react';
+import type { SelectHTMLAttributes } from 'react';
 
-interface SelectBoxProps {
+interface SelectBoxProps extends SelectHTMLAttributes<HTMLSelectElement>{
     children?: React.ReactNode;
     className?: string;
 }
