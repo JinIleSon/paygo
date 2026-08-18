e1c1914c333489d3267b29eb6b6baa6cf263cf8d
sonjinile <sy7521012@naver.com>
2026-08-18 13:03:36 +0900

    refactor: order 타입에 discount 프로퍼티 추가

--- paygo-frontend/src/types/order.ts
+++ paygo-frontend/src/types/order.ts
@@ -1,6 +1,7 @@
-import type { IconName } from "../constants/icons";
+import type { IconName } from '../constants/icons';
 
-export type OrderStatus = 'paymentComplete' | 'shipping' | 'delivered' | 'cancelled' | 'refunded' | 'paymentFailed'; // cancelled는 취소, refunded 취소 후 환불까지
+export type OrderStatus =
+    'paymentComplete' | 'shipping' | 'delivered' | 'cancelled' | 'refunded' | 'paymentFailed'; // cancelled는 취소, refunded 취소 후 환불까지
 export type DeliveryStatus = 'paymentComplete' | 'productReady' | 'shipping' | 'delivered';
 export type PaymentMethod = 'paygo' | 'account' | 'card' | 'simplePayment';
 
@@ -18,23 +19,24 @@ export interface OrderItem {
 }
 
 export interface Order {
-    orderId: string;     // 주문번호
-    createAt: string;    // 주문시간
+    orderId: string; // 주문번호
+    createAt: string; // 주문시간
     orderStatus: OrderStatus; // 주문상태
-    items: OrderItem[];  // 주문한 item 리스트
-    totalPrice: number;  // "할인까지 적용된" 총 금액 - 사용자 개인이 사용한 쿠폰까지 적용된 금액 저장
+    items: OrderItem[]; // 주문한 item 리스트
+    discount: number; // 할인 금액
+    totalPrice: number; // "할인까지 적용된" 총 금액 - 사용자 개인이 사용한 쿠폰까지 적용된 금액 저장
     failureReason?: string; // 실패사유 - 결제실패 시에 존재
-    refundAmount?: number;  // 환불 시에 존재
+    refundAmount?: number; // 환불 시에 존재
 }
 
 export interface OrderDetail extends Order {
     deliveryStatus: DeliveryStatus; // 배송상태
-    recipient: string;              // 수취인
-    recipientAddress: string;       // 수취인 주소
-    recipientPhone: string;         // 수취인 연락처
-    carrier: string;                // 택배사
-    trackingNumber: string;         // 운송장 번호 - 앞이 0이 오는 경우가 있어 string
-    discount: number;               // 할인 금액
-    paymentMethod: PaymentMethod;   // 결제 수단
-    transactionId: string;          // 거래 ID
+    recipient: string; // 수취인
+    recipientAddress: string; // 수취인 주소
+    recipientPhone: string; // 수취인 연락처
+    carrier: string; // 택배사
+    trackingNumber: string; // 운송장 번호 - 앞이 0이 오는 경우가 있어 string
+    discount: number; // 할인 금액
+    paymentMethod: PaymentMethod; // 결제 수단
+    transactionId: string; // 거래 ID
 }
