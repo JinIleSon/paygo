4977e2641f5a38948156159f0467513f79bd0ff3
sonjinile <sy7521012@naver.com>
2026-08-31 12:40:40 +0900

    feat: ReturnRequestModal 모달 컴포넌트 텍스트 색상 변경 및 섹션 간 간격 추가

--- paygo-frontend/src/components/modal/returnRequestModal.tsx
+++ paygo-frontend/src/components/modal/returnRequestModal.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import SelectBox from "../common/selectBox";
-import ModalShell from "./modalShell";
-import Button from "../common/button";
+import { useState } from 'react';
+import SelectBox from '../common/selectBox';
+import ModalShell from './modalShell';
+import Button from '../common/button';
 
 interface ReturnRequestModalProps {
     title: string;
@@ -14,35 +14,58 @@ interface ReturnRequestModalProps {
     onConfirm: (reason: string, detailedReason: string) => void;
 }
 
-function ReturnRequestModal({ title, description, category, text, cancelLabel, confirmLabel, onClose, onConfirm } : ReturnRequestModalProps){
+function ReturnRequestModal({
+    title,
+    description,
+    category,
+    text,
+    cancelLabel,
+    confirmLabel,
+    onClose,
+    onConfirm,
+}: ReturnRequestModalProps) {
     const [reason, setReason] = useState('simple_change');
     const [detailedReason, setDetailedReason] = useState('');
-    
+
     return (
         <ModalShell onClose={onClose}>
             <div className="flex flex-col gap-2">
-                <div>{title}</div>
-                <div className="whitespace-pre-line">{description}</div>
-                <div>{category}</div>
+                <div className="font-medium text-lg">{title}</div>
+                <div className="whitespace-pre-line text-gray-500">{description}</div>
+                <div className="mt-2 font-medium text-gray-600">{category}</div>
                 <div>
-                    <SelectBox value={reason} onChange={(e) => setReason(e.target.value)} className="w-full">
-                        <option value="simple_change">단순 변심</option>
+                    <SelectBox
+                        value={reason}
+                        onChange={(e) => setReason(e.target.value)}
+                        className="w-full"
+                    >
+                        <option value="simple_change">단순변심</option>
                         <option value="wrong_product">상품문제</option>
                         <option value="wrong_delivery">배송문제</option>
                     </SelectBox>
                 </div>
-                <div>{text}</div>
+                <div className="mt-3 font-medium text-gray-600">{text}</div>
                 <div>
                     <textarea
-                        value={detailedReason} 
+                        value={detailedReason}
                         onChange={(e) => setDetailedReason(e.target.value)}
-                        name="" id="" placeholder="구체적인 사유를 입력해주세요."
-                        className="p-3 w-full border border-[#D1D5DB] hover:border-[#C5C8CD] hover:bg-[#F4F4F4] duration-500 rounded-md outline-none h-40">
-                    </textarea>
+                        name=""
+                        id=""
+                        placeholder="구체적인 사유를 입력해주세요."
+                        className="p-3 w-full border border-[#D1D5DB] hover:border-[#C5C8CD] hover:bg-[#F4F4F4] duration-500 rounded-md outline-none h-40"
+                    ></textarea>
                 </div>
                 <div className="flex gap-3 w-full mt-3">
-                    <Button variant="secondary" onClick={onClose} className="w-1/2 h-10">{cancelLabel}</Button>
-                    <Button variant="retry" onClick={() => onConfirm(reason, detailedReason)} className="w-1/2 h-10">{confirmLabel}</Button>
+                    <Button variant="secondary" onClick={onClose} className="w-1/2 h-10">
+                        {cancelLabel}
+                    </Button>
+                    <Button
+                        variant="retry"
+                        onClick={() => onConfirm(reason, detailedReason)}
+                        className="w-1/2 h-10"
+                    >
+                        {confirmLabel}
+                    </Button>
                 </div>
             </div>
         </ModalShell>
