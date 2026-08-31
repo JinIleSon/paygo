b129d39d1440a7ab8480225737d2018c1547168a
sonjinile <sy7521012@naver.com>
2026-09-01 06:53:00 +0900

    feat: 배송 조회 단계 함수 getShippingStep 함수 추가

--- paygo-frontend/src/lib/orderUtils.ts
+++ paygo-frontend/src/lib/orderUtils.ts
@@ -1,3 +1,5 @@
+import type { OrderStatus } from "../types/order";
+
 export function getRemainRefundedDate(createdAt: string): number {
     const refundDate = new Date(createdAt);
     refundDate.setDate(refundDate.getDate() + 7);
@@ -6,4 +8,22 @@ export function getRemainRefundedDate(createdAt: string): number {
     const diffDate = Math.ceil((refundDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24)); // 1000 = 1000밀리초 = 1초
     
     return Math.max(0, diffDate); // 음수가 되면 0으로 매핑되게끔
+}
+
+export function getShippingStep(orderStatus: OrderStatus) {
+    const STEP_OF_ORDER: Record<string, number> = {
+        paymentComplete: 1,
+        shipping: 2,
+        delivered: 3
+    };
+    
+    const step = STEP_OF_ORDER[orderStatus] ?? 0;
+    const labels = ["주문완료", "상품준비", "배송중", "배송완료"];
+
+    // label과 done/current/pending이 따로 놀지 않게 객체로 묶음
+    const result = labels.map((label, i) => ({
+                    label: label,
+                    status: step > i ? "done" : step === i ? "current" : "pending"}));
+
+    return result;
 }
