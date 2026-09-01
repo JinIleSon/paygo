77beafac6437c519a477d1c680516f62b12c5840
sonjinile <sy7521012@naver.com>
2026-09-01 20:37:52 +0900

    feat: 배송 단계를 나타내는 ShippingStep 모달 컴포넌트 추가

---
+++ paygo-frontend/src/components/ship/shippingStep.tsx
@@ -0,0 +1,64 @@
+import { IconCheck, IconHome, IconPackage, IconTruck } from "@tabler/icons-react";
+import { getShippingStep } from "../../lib/orderUtils";
+import type { OrderStatus } from "../../types/order";
+
+interface ShippingStepProps {
+    orderStatus: OrderStatus;
+}
+
+function ShippingStep({ orderStatus } : ShippingStepProps) {
+    const step = getShippingStep(orderStatus); // { label: label, status: "done" 또는 "current" 또는 "pending" }
+    
+    const stepIconColor = step.map((s) => {
+        if (s.status !== "pending")
+            return "bg-blue-500 text-white";
+        else
+            return "bg-gray-200 text-gray-400";
+    });
+    
+    const stepBarColor = step.map((s, index) => {
+        if (index === 0)
+            return undefined;
+        if (s.status !== "pending")
+            return "bg-blue-500";
+        else
+            return "bg-gray-200";
+    });
+
+    const stepTextColor = step.map((s) => {
+        if (s.status !== "current")
+            return "text-gray-400";
+        else
+            return "text-blue-500";
+    });
+
+    const shippingIcon = [
+        IconCheck,
+        IconPackage,
+        IconTruck,
+        IconHome
+    ];
+
+    return (
+        <div className="flex gap-3">
+            {step.map((s, index) => {
+                const Icon = shippingIcon[index];
+                
+                return (
+                <div key={index} className="relative flex flex-col items-center justify-center w-18 gap-1">
+                    <div className={`flex items-center justify-center rounded-full w-8 h-8 ${stepIconColor[index]}`}>
+                        <Icon />
+                    </div>
+                    <div className={`text-sm ${stepTextColor[index]}`}>
+                        {s.label}
+                    </div>
+                    {index !== 0 && (
+                    <div className={`absolute w-14 h-1 top-3.5 right-12 ${stepBarColor[index]}`}></div>
+                    )}
+                </div>
+            )})}
+        </div>
+    );
+}
+
+export default ShippingStep;
