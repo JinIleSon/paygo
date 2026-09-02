8ebb6edb799dc995affb1f0a37bd4d4016d60d5b
sonjinile <sy7521012@naver.com>
2026-09-02 19:35:20 +0900

    feat: 주문 상세 페이지 기본 레이아웃 추가

--- paygo-frontend/src/pages/shopping/orderDetailPage.tsx
+++ paygo-frontend/src/pages/shopping/orderDetailPage.tsx
@@ -1,6 +1,25 @@
+import Card from "../../components/common/card";
+
 function OrderDetailPage() {
     return (
-        <></>
+        <div>
+            <div className="flex gap-8 pr-8">
+                <div className="min-w-1/2">
+                    <div className="flex flex-col gap-6.5 font-medium">
+                        <Card>
+
+                        </Card>
+                    </div>
+                </div>
+                <div className="min-w-1/2">
+                    <div className="flex flex-col gap-4 font-medium">
+                        <Card>
+                            
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
