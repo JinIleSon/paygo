6d8d1eaabd988e54d60b9cc80c4abf9d3a52b922
sonjinile <sy7521012@naver.com>
2026-09-03 07:20:14 +0900

    feat: 잘못된 orderId url에 입력 시 '주문을 찾을 수 없어요.'라는 멘트가 있는 주문 상세 페이지로 이동. + orderId를 기존의 order과 매칭하여 배송조회 카드 추가중

--- paygo-frontend/src/pages/shopping/orderDetailPage.tsx
+++ paygo-frontend/src/pages/shopping/orderDetailPage.tsx
@@ -1,12 +1,35 @@
+import { useParams } from "react-router-dom";
 import Card from "../../components/common/card";
+import { order } from "../../constants/order";
+import { formatDateTime } from "../../lib/dateUtils";
+import { getOrderBadges } from "../../constants/useBadges";
+import { IconAlertTriangle } from "@tabler/icons-react";
 
 function OrderDetailPage() {
+    const { orderId } = useParams<{ orderId: string }>();
+    const orderDetail = order.find((o) => o.orderId === orderId);
+
+    if (!orderDetail)
+        return <div className="fixed inset-0 left-60 flex flex-col items-center justify-center gap-2 text-gray-600">
+                    <IconAlertTriangle size={110} className="text-gray-400"/>
+                    <div className="text-2xl font-medium">주문을 찾을 수 없어요.</div>
+                </div>
+
     return (
         <div>
             <div className="flex gap-8 pr-8">
                 <div className="min-w-1/2">
                     <div className="flex flex-col gap-6.5 font-medium">
-                        <Card>
+                        <Card
+                            className="flex flex-col gap-4 text-gray-400 font-medium"
+                        >
+                            <div className="flex justify-between items-center">
+                                <div>
+                                    <div className="text-sm">주문번호 {orderDetail.orderId}</div>
+                                    <div className="text-xs">{formatDateTime(orderDetail.createdAt)}</div>
+                                </div>
+                                <div>{getOrderBadges(orderDetail.orderStatus)}</div>
+                            </div>
 
                         </Card>
                     </div>
