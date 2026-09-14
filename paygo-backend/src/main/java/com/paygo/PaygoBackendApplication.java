7ecb4aedf8750b7ee3e9241e6956a88a29c46fac
sonjinile <sy7521012@naver.com>
2026-09-15 07:23:14 +0900

    chore: 백엔드 폴더 구조 설정

---
+++ paygo-backend/src/main/java/com/paygo/PaygoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.paygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaygoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaygoBackendApplication.class, args);
+	}
+
+}
