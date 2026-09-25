a3b55168c61fdee3dfc14b9cb02f34b48be71610
sy7521012 <sy7521012@naver.com>
2026-09-25 22:46:42 +0900

    refactor: TransactionHistory 타입 중 type 및 statement 프로퍼티 유니온 타입으로 변경

--- paygo-frontend/src/types/transactionHistory.ts
+++ paygo-frontend/src/types/transactionHistory.ts
@@ -4,10 +4,10 @@ export interface TransactionHistory {
     // TODO: 실제 거래내역 연동 시 orderId 데이터도 함께 추가
     orderId?: string; // FK -> orders 테이블, order_items 테이블, products 테이블을 차례로 조인하여 가져온 뒤 한 달간 사용한 가격 파악(할인까지 추가)
     content: string; // 내용
-    type: string; // 유형('charge', 'buy', 'refund')
+    type: 'charge' | 'buy' | 'refund'; // 유형
     paymentMethod: string; // 결제수단
     amount: number; // 이 거래로 오간 금액
     balance: number; // 이 거래 완료 직후의 잔액
-    statement: string; // 상태('complete', 'fail', 'processing')
+    statement: 'complete' | 'fail' | 'processing'; // 상태
     createdAt: string; // 거래시각
 }
